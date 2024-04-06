868fb264bd3eba4d486ef5655ab2d839ea0fcf1f
Tape27 <avenmoney@gmail.com>
2024-04-07 01:38:51 +0300

    -

--- Src/MultiOperationCalculator.Library/ArrayMathString.cs
+++ Src/MultiOperationCalculator.Library/ArrayMathString.cs
@@ -6,18 +6,16 @@ namespace MultiOperationCalculator.Library
      {
         /// <summary>
         /// Создание нового массива без 2-ух элементов прошлого массива
-        /// Пришел старый массив 5+5+5, так же пришло новое значение и индекс где
-        /// должно находится этот новое значение.
-        /// Метод переписывает старый массив в новый и если итерация = индексу нового элемента,
-        /// то мы подставляем новый элемент в новый массив.
-        /// После мы перескакиваем на 2 элементе вправо и записываем новый массив не как (10+5+5)
-        /// а уже получаем (10+5).
+        /// На вход принимает массив состоящий из чисел и арифметических операций,
+        /// новое значение и индекс куда вставить новое значение
+        /// Пример - input array 5 + 5 + 5, newitem 10, index 0
+        /// Вернёт - array 10 + 5
         /// </summary>
         public static void CreateNewArrayWithResultAndRemoveTwoElements(ref string[] oldArray, string newItem, int indexNewItem)
         {
             if (indexNewItem < 0 || indexNewItem >= oldArray.Length - 2)
             {
-                throw new ArgumentOutOfRangeException("The index has gone beyond the new array");
+                throw new ArgumentOutOfRangeException("The index was outside the new array");
             }
 
             string[] newArray = new string[oldArray.Length - 2];
